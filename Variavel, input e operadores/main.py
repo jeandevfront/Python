28b458c7afb004e2f85f81a83568004362296552
Jean Ferreira <120499171+jeandevfront@users.noreply.github.com>
2024-03-20 06:11:30 -0300

    primeira aula python

---
+++ Variavel, input e operadores/main.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome:")
+idade = int( input("Digite sua idade:") ) # input padrao valor string, com o valor convertido para inteiro #
+peso = float( input("Digite seu peso:") ) # input convertido para float #
+
+print(nome)
+print(type(idade)) # type() retorna tipo da variável
+print(type(peso))
+
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 30 / 3
+potencia = 7 ** 2
+
+print("soma", soma)
+print("multiplicacao", multiplicacao)
+print("divisao", divisao)
+print("potencia", potencia)
